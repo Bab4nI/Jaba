76587475ff34c388ab0d786cf8d2539f25c65636
klawisha012 <klawisha012@gmail.com>
2025-02-21 22:25:15 +0300

    2 версия главной страницы

--- frontend/jaba script/vite.config.js
+++ frontend/jaba script/vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173 // Можно поменять на другой порт
+  },
   plugins: [
     vue(),
     vueDevTools(),
