d473e1554b6c715b0cadaba7677ad314ef3c66ed
klawisha012 <klawisha012@gmail.com>
2025-02-21 20:59:18 +0300

    1 версия главной страницы

--- frontend/jaba script/src/App.vue
+++ frontend/jaba script/src/App.vue
@@ -1,47 +1,145 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
+<!-- html -->
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<div class="container">
+    <!-- header -->
+    <header>
+        <div>NetLab AI</div>
+        <nav>
+            <a href="#">Главная</a>
+            <a href="#">Курс</a>
+            <a href="#">Вход</a>
+        </nav>
+    </header>
+    <!-- main -->
+    <div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
     </div>
-  </header>
+    <div>
 
-  <main>
-    <TheWelcome />
-  </main>
+    </div>  
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+    <!-- footer -->
+    <footer>
+        <div class="left">
+            <p>Контакты команды</p>
+            <nav>
+            <p>Email: <a href="mailto:jabascript@sfedu.com">jabascript@sfedu.com</a></p>
+            <p>Телефон: <a href="tel:+7-(938)-100-16-77">+7-(938)-100-16-77</a></p>
+            <p>Адрес: <a href="https://yandex.ru/maps/?text=пер.+Некрасовский,+44,+г.+Таганрог" target="_blank">пер. Некрасовский, 44, г.Таганрог</a></p>
+        </nav>
+        </div>
+        <div class="right">
+            <p id="partner">Партнеры</p>
+            <img src="@/assets/images/ictis.png" alt="Фото ICTIS" id="ictis">
+        </div>  
+    </footer>
+
+    <hr class="line">
+    <div class="bottom">
+        <p id="netlab">© 2024 NetLab AI. Все права защищены.</p>
+    </div>
+</div>
 </template>
 
+
+
+
+<!-- js -->
+<script setup>
+import { ref, reactive } from 'vue';
+
+const header = 
+{
+    home: {title: 'Главная', link: '/'},
+    course: {title: 'Курс', link: '/course'},
+    login: {title: 'Вход', link: '/login'},
+}
+</script>
+
+
+
+
+<!-- css -->
 <style scoped>
+.container {
+    display: grid;
+    grid-template: repeat(6, minmax(100px, auto)) / 1fr;
+    grid-template-areas:
+    "header"
+    "administration"
+    "advantage"
+    "about"
+    "recomendation"
+    "footer";
+}
+
 header {
-  line-height: 1.5;
+    display: flex;
+    grid-area: header;
+    background-color: #F5F9F8;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header nav {
+    display: flex;
+    column-gap: 2vw;
+    
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+header nav a {
+    text-decoration: none;
+    color: #000;
+    font-weight: bold;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+header nav a:hover {
+    text-decoration: underline #24222F;
+}
 
-  header .wrapper {
+footer {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    grid-area: footer;
+    background-color: #575667;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+}
+
+.left {
+    width: 50%;
+}
+
+.right {
+    width: 50%;
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: column;
+    align-items: flex-end;
+}
+
+.bottom {
+    margin: 0px;
+    background-color: #575667;
+}
+
+.line {
+    margin: 0px 0px;
+    height: 2px;
+    background-color:  #A094B8;
+}
+
+#ictis {
+    max-width: 50%;
+}
+
+#partner {
+    margin-right: 5vw;
 }
 </style>
--- frontend/jaba script/src/main.js
+++ frontend/jaba script/src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@/assets/fonts/fonts.css';
 
 createApp(App).mount('#app')
